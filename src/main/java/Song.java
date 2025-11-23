fa70d7d593a9db636ff75b57f10dd30f43bdba09
Alexandre Queiroz <megalexandre@gmail.com>
2025-11-23 23:14:21 +0000

    create readme

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -6,7 +6,6 @@ public class Song {
             99 bottles of beer on the wall, 99 bottles of beer.
             Take one down and pass it around, 98 bottles of beer on the wall.
             """;
-          
     }
 
     public String verses(int start, int end){
