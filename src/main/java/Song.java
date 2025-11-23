66c5fc21452e5b2019081ec1df1d7de3071d3e73
Alexandre Queiroz <megalexandre@gmail.com>
2025-11-23 14:19:21 +0000

    upgrade strings to text blocs

---
+++ src/main/java/Song.java
@@ -0,0 +1,20 @@
+public class Song {
+
+    public String verse(int num){
+        return 
+            """
+            99 bottles of beer on the wall, 99 bottles of beer.
+            Take one down and pass it around, 98 bottles of beer on the wall.
+            """;
+          
+    }
+
+    public String verses(int start, int end){
+        return "";
+    }
+
+    public String song(){
+        return "";
+    }
+
+}
